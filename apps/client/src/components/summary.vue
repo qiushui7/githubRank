a1ce90715144a0b45532a3f4fec43ca7edebfda7
xiangjiuzhen <2731571882@qq.com>
2024-10-27 22:08:24 +0800

    feat:summary页面

--- apps/client/src/components/summary.vue
+++ apps/client/src/components/summary.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>
-        summary
+    <div class="summary-container">
+        {{content}}
     </div>
 </template>
 <script setup name="summary">
+let content = 'Evan You has 195 repositories on GitHub, the latest 100 with user activity were loaded from GitHub sweb service for this evaluation.Evan You has pushed to 92 of these repositories. This is a high ratio congratulations!'
 </script>
+<style>
+.summary-container{
+    width: 70%;
+    margin-top: 30px;
+    margin-left: 10%;
+    border: 1px solid red;
+    height: 70%;
+    font-size: 30px;
+    padding-left: 15px;
+}
+</style>
