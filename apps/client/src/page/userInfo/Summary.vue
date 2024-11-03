aba1d64d286067d73f8218cb9862323b1fc922bd
xiangjiuzhen <2731571882@qq.com>
2024-11-03 14:09:30 +0800

    feat: summary页面数据

--- apps/client/src/page/userInfo/Summary.vue
+++ apps/client/src/page/userInfo/Summary.vue
@@ -1,11 +1,45 @@
 <template>
-  <div class="summary-container">
+  <div
+    v-if="show"
+    style="width: 44vw; margin-top: 3vh; margin-left: 5vw; height: 40vh"
+  >
+    <div id="tiny-basic-loading1" style="margin-top: 20vh"></div>
+  </div>
+  <div
+    class="work-introduction"
+    style="
+      width: 44vw;
+      margin-top: 3vh;
+      margin-left: 5vw;
+      font-size: 25px;
+      line-height: 40px;
+    "
+    v-if="!show"
+  >
     {{ content }}
   </div>
 </template>
 <script setup name="summary">
-let content =
-  'Evan You has 195 repositories on GitHub, the latest 100 with user activity were loaded from GitHub sweb service for this evaluation.Evan You has pushed to 92 of these repositories. This is a high ratio congratulations!';
+import { ref, onMounted } from 'vue';
+import { TinyLoading, TinyButton } from '@opentiny/vue';
+const loadingInstance = ref(null);
+const closeLoading = () => {
+  loadingInstance.value.close();
+};
+onMounted(() => {
+  loadingInstance.value = TinyLoading.service({
+    target: document.getElementById('tiny-basic-loading1'),
+    size: 'large',
+  });
+});
+fetch('/api/info/summary?github_id=yyx990803').then((res) =>
+  res.json().then((data) => {
+    content.value = data.summary;
+    show.value = false;
+  }),
+);
+let content = ref('');
+let show = ref(true);
 </script>
 <style>
 /* .summary-container{
@@ -17,4 +51,18 @@ let content =
     font-size: 30px;
     padding-left: 15px;
 } */
+.work-introduction {
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+p {
+  line-height: 1.6;
+  color: #555;
+}
 </style>
